9c2dfd9d5617b812074e523ab5837eee6c0c0ed4
wesleylam1 <48534350+wesleylam1@users.noreply.github.com>
2021-06-23 22:43:30 -0700

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LaptopIcon from "@material-ui/icons/Laptop";
+
+function Footer() {
+  return (
+    <footer>
+      <LaptopIcon />
+      <p>Wesley Lam </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CollectionsIcon from "@material-ui/icons/Collections";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <CollectionsIcon />
+        Note-Taking App
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
